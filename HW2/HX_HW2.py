8db00d27f4871d6f490e5df3702d3e8003d8402b
Hui Xia <huixia@bmeadmins-MacBook-Pro.local>
2020-01-28 00:02:45 -0600

    SDfdsf

--- HW2/HX_HW2.py
+++ HW2/HX_HW2.py
@@ -120,11 +120,11 @@ if __name__ == '__main__':
 
     temp_list = []
     alter_list = []
-    for i in np.linspace(0, 1, num=50):
+    for i in np.linspace(-1, 1, num=50):
         alter_list.append(i)
-        temp_list.append(cal_TD(probToState=0.22,
-                                valueEstimates=[0.0, -5.2, 0.0, 25.4, 10.6, 9.2, 12.3],
-                                rewards=[-2.4, 0.8, 4.0, 2.5, 8.6, -6.4, 6.1],
+        temp_list.append(cal_TD(probToState=0.64,
+                                valueEstimates=[0.0, 4.9, 7.8, -2.3, 25.5, -10.2, -6.5],
+                                rewards=[-2.4, 9.6, -7.8, 0.1, 3.4, -2.1, 7.9],
                                 lambd=i,
                                 gamma=1))
 
